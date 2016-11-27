65cd2aa72b65258157eba7269ff1ed9c2496685c
mkollers <mail@markuskollers.de>
2016-11-27 02:49:19 +0100

    improve tests

--- tests/config/routes.spec.ts
+++ tests/config/routes.spec.ts
@@ -3,6 +3,9 @@
 module MyHappyNails {
     describe('routes', (): void => {
         let $urlRouterProvider: ng.ui.IUrlRouterProvider;
+        let $state: ng.ui.IStateService;
+        let $location: ng.ILocationService;
+        let $rootScope: ng.IRootScopeService;
 
         beforeEach((): void => {
             angular.module('urlRouterProviderConfig', ['ui.router', 'my-happy-nails.sidenav'])
@@ -15,8 +18,86 @@ module MyHappyNails {
             inject();
         });
 
+        beforeEach(inject((_$state_: ng.ui.IStateService, _$location_: ng.ILocationService, _$rootScope_: ng.IRootScopeService): void => {
+            $state = _$state_;
+            $location = _$location_;
+            $rootScope = _$rootScope_;
+        }));
+
         it('should set default route to root', (): void => {
             expect($urlRouterProvider.otherwise).toHaveBeenCalledWith('/');
+        });
+
+        it('should go to /', (): void => {
+            // Arrange
+            $state.go('base.home');
+
+            // Act
+            $rootScope.$digest();
+
+            // Assert
+            expect($location.url()).toEqual('/');
+            expect($state.current.data.header).toEqual('Home');
+        });
+
+        it('should go to /Preise', (): void => {
+            // Arrange
+            $state.go('base.prices');
+
+            // Act
+            $rootScope.$digest();
+
+            // Assert
+            expect($location.url()).toEqual('/Preise');
+            expect($state.current.data.header).toEqual('Preise');
+        });
+
+        it('should go to /Bilder', (): void => {
+            // Arrange
+            $state.go('base.images');
+
+            // Act
+            $rootScope.$digest();
+
+            // Assert
+            expect($location.url()).toEqual('/Bilder');
+            expect($state.current.data.header).toEqual('Bilder');
+        });
+
+        it('should go to /Kontakt', (): void => {
+            // Arrange
+            $state.go('base.contact');
+
+            // Act
+            $rootScope.$digest();
+
+            // Assert
+            expect($location.url()).toEqual('/Kontakt');
+            expect($state.current.data.header).toEqual('Kontakt');
+        });
+
+        it('should go to /Anfahrt', (): void => {
+            // Arrange
+            $state.go('base.location');
+
+            // Act
+            $rootScope.$digest();
+
+            // Assert
+            expect($location.url()).toEqual('/Anfahrt');
+            expect($state.current.data.header).toEqual('Anfahrt');
+        });
+
+        it('should go to /Impressum', (): void => {
+            // Arrange
+            $state.go('base.imprint');
+
+            // Act
+            $rootScope.$digest();
+
+            // Assert
+            expect($location.url()).toEqual('/Impressum');
+            expect($state.current.data.header).toEqual('Impressum');
         });
     });
 }
