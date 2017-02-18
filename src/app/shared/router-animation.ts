ea039908e30195ff7425acc79b10c8bf56c0c3ad
mkollers <mail@markuskollers.de>
2017-02-18 17:43:38 +0100

    fixed footer position

--- src/app/shared/router-animation.ts
+++ src/app/shared/router-animation.ts
@@ -6,14 +6,12 @@ export function RouterTransition() {
 
 function slideToLeft() {
     return trigger('routerTransition', [
-        state('void', style({ width: '100%', position: 'absolute' })),
-        state('*', style({ width: '100%', position: 'absolute' })),
-        transition(':enter', [
-            style({ transform: 'translateX(100%)' }),
+        transition('void => *', [
+            style({ position: 'absolute', width: '100%', height: '100%', transform: 'translateX(100%)' }),
             animate('0.5s ease-in-out', style({ transform: 'translateX(0%)' }))
         ]),
-        transition(':leave', [
-            style({ transform: 'translateX(0%)' }),
+        transition('* => void', [
+            style({ position: 'absolute', width: '100%', height: '100%', transform: 'translateX(0%)' }),
             animate('0.5s ease-in-out', style({ transform: 'translateX(-100%)' }))
         ])
     ]);
