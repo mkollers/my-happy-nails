7eece9d965ff15e5aa601665cbca427b611a7ee4
Markus Kollers <m.kollers@vertical.de>
2018-11-01 21:51:09 +0100

    Change album fotos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   facebook: {
-    albumId: 1103187183159849,
+    albumId: 1605110389634190,
     appId: 1230739466991756,
     appSecret: 'd9d8fe2519ea59b0fdd07964b0fe52e0'
   }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@
 export const environment = {
   production: false,
   facebook: {
-    albumId: 1103187183159849,
+    albumId: 1605110389634190,
     appId: 1230739466991756,
     appSecret: 'd9d8fe2519ea59b0fdd07964b0fe52e0'
   }
