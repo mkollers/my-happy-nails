1900aeed1be0f302ac35dff93f418cb99106aedd
Markus Kollers <m.kollers@vertical.de>
2019-05-18 23:07:16 +0200

    Refactor home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,20 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
-import { join } from 'lodash';
 
 import { HeaderService } from '../shared/layout/services/header.service';
-import { Address } from '../shared/models/address';
-import { INITIAL_STORE_DATA } from '../shared/store/store-data';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent implements OnInit {
-  address: Address;
-  phone: string;
-  mail: string;
   showHolidays = false;
 
   constructor(
@@ -22,10 +17,9 @@ export class HomeComponent implements OnInit {
     private _header: HeaderService,
     private _title: Title
   ) {
-    this.setData();
-    this.setSeoData();
+    this._setSeoData();
 
-    const end = new Date(2019, 5, 13);
+    const end = new Date(2019, 4, 13);
     const now = new Date();
     if (now < end) {
       this.showHolidays = true;
@@ -38,29 +32,29 @@ export class HomeComponent implements OnInit {
 
   createMap() {
     const map = new google.maps.Map(document.getElementById('map'), {
-      center: INITIAL_STORE_DATA.location,
+      center: {
+        lat: 50.133442,
+        lng: 8.5351918
+      },
       zoom: 17,
       zoomControl: false,
       disableDefaultUI: true
     });
 
-    const marker = new google.maps.Marker({
+    new google.maps.Marker({
       animation: google.maps.Animation.BOUNCE,
-      position: INITIAL_STORE_DATA.location,
+      position: {
+        lat: 50.133442,
+        lng: 8.5351918
+      },
       map: map
     });
   }
 
-  private setData() {
-    this.address = INITIAL_STORE_DATA.address;
-    this.phone = INITIAL_STORE_DATA.phone;
-    this.mail = INITIAL_STORE_DATA.mail;
-  }
-
-  private setSeoData() {
+  private _setSeoData() {
     this._header.title = 'Home';
     this._title.setTitle('Günstiges Nagelstudio für schöne Fingernägel in Sulzbach');
-    this._meta.updateTag({ name: 'description', content: 'Professionelles Nagelstudio zu fairen Preisen in Sulzbach. Du suchst einen Profi für deine Nägel zu günstigen Preisen? Kunstnägel, Gelnägel, Maniküre, Nailart' })
-    this._meta.updateTag({ name: 'keywords', content: join(['nagelstudio', 'nageldesign', 'sulzbach', 'öffnungszeiten', 'maniküre', 'kontakt', 'adresse'], ',') })
+    this._meta.updateTag({ name: 'description', content: 'Professionelles Nagelstudio zu fairen Preisen in Sulzbach. Du suchst einen Profi für deine Nägel zu günstigen Preisen? Kunstnägel, Gelnägel, Maniküre, Nailart' });
+    this._meta.updateTag({ name: 'keywords', content: 'nagelstudio,nageldesign,sulzbach,öffnungszeiten,maniküre,kontakt,adresse' });
   }
 }
