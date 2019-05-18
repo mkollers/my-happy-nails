7464d18c1b22f101987fb40329018d555e0b4db5
Markus Kollers <m.kollers@vertical.de>
2019-05-18 22:53:37 +0200

    Improve sidenav design

--- src/app/shared/layout/services/header.service.ts
+++ src/app/shared/layout/services/header.service.ts
@@ -5,7 +5,7 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root'
 })
 export class HeaderService {
-  title$ = new BehaviorSubject<string>('Test');
+  title$ = new BehaviorSubject<string>('');
 
   constructor() { }
 
