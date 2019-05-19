247d289a2792c51f4f33a7fd49fb015e2e6d7fb8
Markus Kollers <m.kollers@vertical.de>
2019-05-19 19:36:42 +0200

    Add lazy loading and some new images

--- src/app/shared/layout/layout.module.ts
+++ src/app/shared/layout/layout.module.ts
@@ -3,12 +3,11 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
 
+import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
-import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -28,8 +27,7 @@ import { FooterComponent } from './components/footer/footer.component';
     // Material
     MatButtonModule,
     MatIconModule,
-    MatSidenavModule,
-    MatToolbarModule
+    MatSidenavModule
   ]
 })
 export class LayoutModule { }
