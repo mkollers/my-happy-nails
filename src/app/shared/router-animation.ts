b372e4b455d3df74b4decd1b2286523a361032c5
Markus Kollers <m.kollers@vertical.de>
2019-05-19 18:46:17 +0200

    Change animation

--- src/app/shared/router-animation.ts
+++ src/app/shared/router-animation.ts
@@ -2,18 +2,17 @@ import { animate, group, query, style, transition, trigger } from '@angular/anim
 
 export const RouterTransition = trigger('routerTransition', [
     transition('* <=> *', [
-    /* order */
-    /* 1 */ query(':enter, :leave', style({ position: 'fixed', width: '100%' })
-            , { optional: true }),
-    /* 2 */ group([  // block executes in parallel
-            query(':enter', [
-                style({ transform: 'translateX(100%)' }),
-                animate('0.5s ease-in-out', style({ transform: 'translateX(0%)' }))
-            ], { optional: true }),
-            query(':leave', [
-                style({ transform: 'translateX(0%)' }),
-                animate('0.5s ease-in-out', style({ transform: 'translateX(-100%)' }))
-            ], { optional: true })
-        ])
+        query(':enter', [
+            style({ opacity: 0 })
+        ], { optional: true }
+        ), query(':leave', [
+            style({ opacity: 1 }),
+            animate('0.3s', style({ opacity: 0 }))
+        ], { optional: true }
+        ), query(':enter', [
+            style({ opacity: 0 }),
+            animate('0.3s', style({ opacity: 1 }))
+        ], { optional: true }
+        )
     ])
 ]);
