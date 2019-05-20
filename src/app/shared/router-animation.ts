1cfbce01a5218fb14ed8ce4eaa6559f05a45abf4
Markus Kollers <m.kollers@vertical.de>
2019-05-20 12:20:33 +0200

    Remove leaving animation

--- src/app/shared/router-animation.ts
+++ src/app/shared/router-animation.ts
@@ -7,7 +7,7 @@ export const RouterTransition = trigger('routerTransition', [
         ], { optional: true }
         ), query(':leave', [
             style({ opacity: 1 }),
-            animate('0.3s', style({ opacity: 0 }))
+            animate('0s', style({ opacity: 0 }))
         ], { optional: true }
         ), query(':enter', [
             style({ opacity: 0 }),
